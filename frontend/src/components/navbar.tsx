4fcace84c8504197c2108fac0aaee20541f2aad7
Aleksandar Rangelov <4brn@proton.me>
2025-03-28 14:14:39 +0200

    mobile friendly navbar

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -2,57 +2,103 @@ import ThemeToggle from "@/components/theme-toggle";
 import { useAuth } from "@/context/auth-context";
 import Logout from "@/components/auth/logout";
 import { Button } from "@/components/ui/button";
-import { Link } from "react-router";
+import { Link, NavLink } from "react-router";
 
 import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuItem,
-  NavigationMenuList,
-  NavigationMenuTrigger,
+    NavigationMenu,
+    NavigationMenuContent,
+    NavigationMenuItem,
+    NavigationMenuList,
+    NavigationMenuTrigger,
 } from "@/components/ui/navigation-menu";
-import { House, File } from "lucide-react";
+import { House, File, Menu } from "lucide-react";
 
 export default function Navbar() {
-  const { user } = useAuth()!;
-  return (
-    <nav className="flex justify-between items-center sticky top-0 z-50 p-2 bg-background">
-      <Link to={"/"}>
-        <Button
-          variant={"link"}
-          className="scroll-m-20 text-2xl font-bold tracking-tight"
-        >
-          StddyBddy{user && `@${user.username}`}
-        </Button>
-      </Link>
-
-      {user && (
-        <NavigationMenu>
-          <NavigationMenuList>
-            <NavigationMenuItem>
-              <Link to="/dashboard">
-                <Button variant={"ghost"} className="text-lg hover:bg-accent">
-                  <House strokeWidth={2} />
-                  Dashboard
+    const { user } = useAuth()!;
+    return (
+        <nav className="flex justify-center items-center sticky top-0 z-50 p-2 bg-background @xs:justify-evenly sm:justify-between">
+            <Link to={"/"}>
+                <Button
+                    variant={"link"}
+                    className="text-2xl font-bold tracking-tight"
+                >
+                    StddyBddy{user && `@${user.username}`}
                 </Button>
-              </Link>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <Link to="/tests">
-                <Button variant={"ghost"} className="text-lg hover:bg-accent">
-                  <File strokeWidth={2} />
-                  Tests
-                </Button>
-              </Link>
-            </NavigationMenuItem>
-          </NavigationMenuList>
-        </NavigationMenu>
-      )}
+            </Link>
 
-      <div className="flex gap-2">
-        <ThemeToggle />
-        {user && <Logout />}
-      </div>
-    </nav>
-  );
+            {user ? (
+                <NavigationMenu orientation="vertical" className="sm:hidden">
+                    <NavigationMenuList>
+                        <NavigationMenuItem>
+                            <NavigationMenuTrigger>
+                                <Menu strokeWidth={3} />
+                            </NavigationMenuTrigger>
+                            <NavigationMenuContent className="flex flex-col gap-1">
+                                <Link to="/dashboard">
+                                    <Button
+                                        variant={"secondary"}
+                                        className="hover:bg-accent"
+                                    >
+                                        <House strokeWidth={3} />
+                                        Dashboard
+                                    </Button>
+                                </Link>
+                                <Link to="/tests">
+                                    <Button
+                                        variant={"secondary"}
+                                        className="w-full hover:bg-accent"
+                                    >
+                                        <File strokeWidth={3} />
+                                        Tests
+                                    </Button>
+                                </Link>
+                                <div className="flex w-full gap-1">
+                                    <ThemeToggle />
+                                    <Logout />
+                                </div>
+                            </NavigationMenuContent>
+                        </NavigationMenuItem>
+                    </NavigationMenuList>
+                </NavigationMenu>
+            ) : (
+                <div className="sm:hidden">
+                    <ThemeToggle />
+                </div>
+            )}
+
+            {user && (
+                <NavigationMenu className="hidden sm:block">
+                    <NavigationMenuList>
+                        <NavigationMenuItem>
+                            <Link to="/dashboard">
+                                <Button
+                                    variant={"ghost"}
+                                    className="text-lg hover:bg-accent"
+                                >
+                                    <House strokeWidth={3} />
+                                    Dashboard
+                                </Button>
+                            </Link>
+                        </NavigationMenuItem>
+                        <NavigationMenuItem>
+                            <NavLink to="/tests">
+                                <Button
+                                    variant={"ghost"}
+                                    className="text-lg hover:bg-accent"
+                                >
+                                    <File strokeWidth={3} />
+                                    Tests
+                                </Button>
+                            </NavLink>
+                        </NavigationMenuItem>
+                    </NavigationMenuList>
+                </NavigationMenu>
+            )}
+
+            <div className="hidden sm:flex gap-2 ">
+                <ThemeToggle />
+                {user && <Logout />}
+            </div>
+        </nav>
+    );
 }
--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -3,22 +3,22 @@ import { useAuth } from "@/context/auth-context";
 import { Link } from "react-router";
 
 export default function Home() {
-  const { user } = useAuth()!;
-  return (
-    <div className="flex min-h-svh items-center justify-center p-6 md:p-10">
-      <div className="flex flex-col items-center justify-center gap-5">
-        <h1 className="font-bold leading-tight text-5xl">
-          Welcome {user ? user.username : "to StddyBddy"}
-        </h1>
-        <p className="text-xl text-accent-foreground">
-          Become an academic weapon
-        </p>
-        <Link to={user ? "/dashboard" : "/auth"}>
-          <Button size={"lg"} className="text-xl p-6">
-            Learn
-          </Button>
-        </Link>
-      </div>
-    </div>
-  );
+    const { user } = useAuth()!;
+    return (
+        <div className="flex min-h-svh items-center justify-center p-6 md:p-10">
+            <div className="flex flex-col items-center justify-center gap-5">
+                <h1 className="font-bold text-center leading-tight text-5xl">
+                    Welcome {user ? user.username : "to StddyBddy"}
+                </h1>
+                <p className="text-xl text-center text-accent-foreground">
+                    Become an academic weapon
+                </p>
+                <Link to={user ? "/dashboard" : "/auth"}>
+                    <Button size={"lg"} className="text-xl p-6">
+                        Learn
+                    </Button>
+                </Link>
+            </div>
+        </div>
+    );
 }
