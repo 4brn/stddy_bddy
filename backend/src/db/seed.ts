a393e843476fe0ef7a9ebaf9042fc4d080b90e91
Aleksandar Rangelov <4brn@proton.me>
2025-03-28 22:35:28 +0200

    lower the number of seeded users and tests

--- backend/src/db/seed.ts
+++ backend/src/db/seed.ts
@@ -4,79 +4,81 @@ import { password as p } from "bun";
 import { exit } from "process";
 
 async function hash(password: string) {
-    return await p.hash(password, "bcrypt");
+  return await p.hash(password, "bcrypt");
 }
 
 const seedUsers = async () => {
-    const users: UserInsert[] = [];
+  const users: UserInsert[] = [];
+  users.push({
+    username: "admin",
+    password: await hash("admin"),
+    role: "admin",
+  });
+  users.push({
+    username: "deleted_user",
+    password: await hash("deleted_user"),
+    role: "admin",
+  });
+  for (let i = 1; i <= 5; i++) {
+    const user: UserInsert = {
+      username: `user${i}`,
+      password: await hash(`user${i}`),
+      role: "user",
+    };
+    users.push(user);
+  }
 
-    for (let i = 1; i <= 10; i++) {
-        const user: UserInsert = {
-            username: `user${i}`,
-            password: `user${i}`,
-            role: "user",
-        };
-        user.password = await hash(user.password);
-
-        users.push(user);
-    }
-
-    await db.insert(usersTable).values(users);
-    console.info("Users seed: Success");
+  await db.insert(usersTable).values(users);
+  console.info("Users seed: Success");
 };
 
 const seedTests = async () => {
-    const tests: TestInsert[] = [];
-    const questions: Question[] = [];
+  const tests: TestInsert[] = [];
+  const questions: Question[] = [];
 
-    for (let i = 1; i <= 5; i++) {
-        questions.push({
-            id: i,
-            text: `Question ${i}?`,
-            answers: [
-                { id: 1, value: 10 },
-                { id: 2, value: 1000 },
-                { id: 3, value: 100000 },
-                { id: 4, value: "infinity" },
-            ],
-            correctId: 1,
-        });
-    }
+  for (let i = 1; i <= 3; i++) {
+    questions.push({
+      id: i,
+      text: `Question ${i}?`,
+      answers: [
+        { id: 1, value: 100 },
+        { id: 2, value: 1000 },
+        { id: 3, value: 100000 },
+        { id: 4, value: "infinity" },
+      ],
+      correctId: 1,
+    });
+  }
 
-    for (let i = 1; i < +10; i++) {
-        tests.push({
-            author_id: i,
-            title: `Test ${i}`,
-            is_private: false,
-            questions: [...questions],
-            created_at: new Date(),
-            updated_at: new Date(),
-        });
-    }
+  for (let i = 1; i < 5; i++) {
+    tests.push({
+      author_id: i,
+      title: `Test ${i}`,
+      is_private: false,
+      questions: [...questions],
+      created_at: new Date(),
+      updated_at: new Date(),
+    });
+  }
 
-    await db.insert(testsTable).values(tests);
-    console.info("Tests seed: Success");
+  await db.insert(testsTable).values(tests);
+  console.info("Tests seed: Success");
 };
 
 const users = prompt("Seed users y/n? ");
-if (users?.toLowerCase() === "y") {
-    try {
-        await seedUsers();
-    } catch (err) {
-        console.error("User Seed Error");
-    }
-} else {
-    console.info("User seed skipped");
-    exit();
+if (users && users.toLowerCase() === "y") {
+  try {
+    await seedUsers();
+  } catch (err) {
+    console.error("User Seed Error");
+  }
 }
+
 const tests = prompt("Seed tests y/n? ");
-if (tests?.toLowerCase() === "y") {
-    try {
-        await seedTests();
-    } catch (err) {
-        console.error("Test Seed Error");
-    }
-} else {
-    console.info("Test seed skipped");
-    exit();
+if (tests && tests.toLowerCase() === "y") {
+  try {
+    await seedTests();
+  } catch (err) {
+    console.error("Test Seed Error");
+  }
 }
